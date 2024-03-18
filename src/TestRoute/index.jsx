9ac5c80e52ca9039ca68829369a8813a94d4ed37
iigmir <roc120j@gmail.com>
2024-03-18 09:38:07 +0800

    Extract <ComponentEntry />

--- src/TestRoute/index.jsx
+++ src/TestRoute/index.jsx
@@ -1,30 +1,15 @@
-// FontAwesome
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faCoffee } from "@fortawesome/free-solid-svg-icons"
 // Local components
 import TheModal from "./Modal";
 import RatingItem from "./RatingItem";
-// CommentModal
-import CommentModal from "../components/CommentModal/index";
-import { modal_modules } from "../components/CommentModal/utils";
-// react-bootstrap
-import Button from "react-bootstrap/Button";
+import ComponentEntry from "./ComponentEntry";
 
 function TestRoute() {
-    const { show, show_modal, close_modal } = modal_modules();
     return <div id="test-route" className="m-2">
         <h1>Developer's console</h1>
-        <p>
-            <cite>Hello Dolly</cite>:
-            <FontAwesomeIcon icon={faCoffee} />
-        </p>
-        <Button variant="primary" size="sm" onClick={show_modal}>Comment</Button>
-        <div className="modal">
-            <CommentModal show={show} handleClose={close_modal} iid="1" />
-        </div>
+        <ComponentEntry />
         <RatingItem />
         <TheModal />
-    </div>
+    </div>;
 }
 
 export default TestRoute;
