6c3afcd1049deecbecaefffb8ee7dfebeb269d7d
Mingzhen0814 <fcum1102930@gmail.com>
2024-03-20 00:17:07 +0800

    清單首頁未完成

--- src/UserListThings/Test.js
+++ src/UserListThings/Test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import LogoutBar from '../MemberSystem/LogoutBar';
 import { Container, Row, Col, Nav, Navbar, Form, Card } from 'react-bootstrap';
-
+import Image from 'react-bootstrap/Image';
 
 
 
@@ -10,79 +10,39 @@ import { Container, Row, Col, Nav, Navbar, Form, Card } from 'react-bootstrap';
 const TravelApp = () => {
   return (
     <>
-    <LogoutBar></LogoutBar>
-    <Container fluid className="bg-light min-vh-100 d-flex flex-column">
-      <Row className="flex-grow-1">
-        <Col md={3} className="p-4" style={{backgroundColor:'white'}}>
-          <div className="mb-4 text-dark">金城武</div>
-          <div className="mb-2 text-dark">未完成</div>
-          <div className="mb-2 text-dark">已完成</div>
-          <div className="mb-4 text-dark font-weight-bold">{}</div>
-          <div className="mb-2 text-dark">臺南之旅</div>
-        </Col>
-
-        <Col md={9} className="pt-8" style={{backgroundColor:'#f9f7c9'}}>
-          <h2 className="mb-4">來趟盡興的旅行吧!</h2>
-          <Row className="mb-4">
-            <Col md={4}>
-              <Card.Img src="https://images.pexels.com/photos/663487/pexels-photo-663487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Beach" className="rounded" />
-            </Col>
-            <Col md={4}>
-              <Card.Img src="https://images.pexels.com/photos/663487/pexels-photo-663487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Old Town" className="rounded" />
-            </Col>
-            <Col md={4}>
-              <Card.Img src="https://images.pexels.com/photos/663487/pexels-photo-663487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Cityscape" className="rounded" />
-            </Col>
-          </Row>
-          <div className="mt-8">
-            <h3 className="mb-2">開始建立旅行 to do list</h3>
-            <Form.Control
-              type="text"
-              placeholder="沒想法嗎?想也可以看看其他人的規劃..."
-              className="border border-secondary rounded px-4 py-2"
-            />
-          </div>
-          <Row className="mt-8">
-            <Col md={3}>
-              <Card>
-                <Card.Img
-                  variant="top"
-                  src="https://images.pexels.com/photos/663487/pexels-photo-663487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                  alt="City Tour"
-                />
-                <Card.Body>
-                  <Card.Text>城市旅遊</Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col md={3}>
-              <Card>
-                <Card.Img
-                  variant="top"
-                  src="https://images.pexels.com/photos/663487/pexels-photo-663487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                  alt="Mountain Hiking"
-                  />
-                <Card.Body>
-                <Card.Text>遊覽山水</Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col md={3}>
-              <Card>
-                <Card.Img
-                  variant="top"
-                  src="https://images.pexels.com/photos/663487/pexels-photo-663487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                  alt="Cultural Tour"
-                />
-                <Card.Body>
-                  <Card.Text>文藝旅行</Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-    </Container>
+      <LogoutBar></LogoutBar>
+      <Container fluid className='vh-100' >
+        <Row className='h-100'>
+          <Col sm={3}>
+            <Row style={{ alignItems: 'center' }}>
+              <Col>
+                <Image className='m-4' src='https://images.pexels.com/photos/20147042/pexels-photo-20147042.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' roundedCircle style={{ border: 'grey', width: '150px', height: '150px' }}></Image>
+              </Col>
+              <Col>
+                <p style={{ fontSize: "32px" }}> userName</p>
+              </Col>
+            </Row>
+            <Row className='m-4' style={{ fontSize: "32px" }}>
+              <Col>未完成</Col>
+              <Col>已完成</Col>
+            </Row>
+            <Row className='m-4'>
+              <Col>
+                <Card>
+                  <Card.Body>
+                    <Card.Text>
+                      我的壯遊之旅
+                    </Card.Text>
+                  </Card.Body>
+                  <Card.Img variant="bottom" src="https://images.pexels.com/photos/17573850/pexels-photo-17573850.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" style={{ border: 'grey', width: '100%', height: '300px' }} />
+                </Card>
+              </Col>
+            </Row>
+          </Col>
+          <Col sm={6} style={{ backgroundColor: '#F9F7C9' }}>2 of 2</Col>
+          <Col sm={3}>3 of 2</Col>
+        </Row >
+      </Container >
     </>
   );
 };
