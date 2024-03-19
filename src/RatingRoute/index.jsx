2db5bd91c64fc303ef707934a0ccb6dfc3b7631b
iigmir <roc120j@gmail.com>
2024-03-19 09:59:09 +0800

    Refactor components

--- src/RatingRoute/index.jsx
+++ src/RatingRoute/index.jsx
@@ -1,15 +1,53 @@
-import ComponentEntry from "./ComponentEntry";
-import AttractionAndProject from "./AttractionAndProject";
-import CreateComment from "./CreateComment";
+import CommentModal from "../components/CommentModal/index";
+import { modal_modules } from "../components/CommentModal/utils";
+// react-bootstrap
+import Button from "react-bootstrap/Button";
+import { useState, useEffect } from "react";
 
-function TestRoute() {
-    return <div id="test-route" className="m-2">
-        <h1>Developer's console</h1>
-        <ComponentEntry />
-        <hr />
-        <AttractionAndProject />
-        <CreateComment />
+async function get_project_api() {
+    const r = await fetch(
+        `${process.env.REACT_APP_API_URL}/api/project`,
+        { method: "GET", }
+    );
+    return await r.json();
+}
+
+function PidSelector({ change_action }) {
+    const [projects, set_projects] = useState([]);
+    const get_projects = () => {
+        get_project_api().then( (response) => {
+            set_projects(response.result);
+        });
+    };
+    useEffect(() => {
+        let mounted = true;
+        if( mounted ) {
+            get_projects();
+        }
+        return () => mounted = false;
+    }, []);
+    const it_comp = its => <option key={its.pid} value={its.pid}>{its.pname} at {its.aid}</option>;
+    const ca_actn = (ev) => change_action(ev.target.value);
+    return <select name="pid" onChange={ca_actn}>{projects.map(it_comp)}</select>;
+}
+
+function ComponentEntry() {
+    const [pid, set_pid] = useState(1);
+    const { show, show_modal, close_modal } = modal_modules();
+    return <div className="asd">
+        <PidSelector change_action={set_pid} />
+        <Button variant="primary" size="sm" onClick={show_modal}>Comment</Button>
+        <div className="modal">
+            <CommentModal show={show} handleClose={close_modal} pid={pid} />
+        </div>
     </div>;
 }
 
-export default TestRoute;
+function RatingRoute() {
+    return <div id="test-route" className="m-2">
+        <h1>評價系統</h1>
+        <ComponentEntry />
+    </div>;
+}
+
+export default RatingRoute;
