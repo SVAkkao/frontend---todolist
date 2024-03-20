34e04eaf1ad141a572fb7cca9e00798c70e48c66
Areil0317 <qscft502832@gmail.com>
2024-03-20 10:26:14 +0800

    更改圖片網址

--- src/MemberSystem/MemberCentre/ChangeAvatar.js
+++ src/MemberSystem/MemberCentre/ChangeAvatar.js
@@ -5,20 +5,8 @@ const API_IMAGE = process.env.REACT_APP_IMAGE_URL;
 
 function ChangeAvatar() {
   const [avatar, setAvatar] = useState(null);
-  const [previewUrl, setPreviewUrl] = useState(
-    "https://via.placeholder.com/150"
-  );
+  const [previewUrl, setPreviewUrl] = useState();
   const [users, setUsers] = useState([]);
-
-  const handleChange = (event) => {
-    const file = event.target.files[0];
-    if (file && file.type.substr(0, 5) === "image") {
-      setAvatar(file);
-      setPreviewUrl(URL.createObjectURL(file));
-    } else {
-      setAvatar(null);
-    }
-  };
 
   useEffect(() => {
     const token = localStorage.getItem("userToken");
@@ -33,13 +21,23 @@ function ChangeAvatar() {
       })
         .then((response) => response.json())
         .then((data) => {
-          setUsers(data.users); // 假設API響應中包含一個名為users的數組
+          setUsers(data); // 假設API響應中包含一個名為users的數組
+          setPreviewUrl(`${API_IMAGE}${data.photo}`); // 设置初始图片预览URL
         })
         .catch((error) => console.error("Fetching data error: ", error));
     } else {
       console.log("Token not found");
     }
   }, []);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      URL.revokeObjectURL(previewUrl);
+      const fileUrl = URL.createObjectURL(file);
+      setPreviewUrl(fileUrl);
+    }
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -63,8 +61,7 @@ function ChangeAvatar() {
         }}
       >
         <img
-          src={`${API_IMAGE}${users.photo}`}
-          alt={`User ${users.id}`}
+          src={previewUrl}
           style={{ width: "100%", height: "100%", objectFit: "contain" }}
         />
         {/* <img
@@ -74,7 +71,7 @@ function ChangeAvatar() {
         /> */}
       </div>
       <form onSubmit={handleSubmit}>
-        <input type="file" onChange={handleChange} />
+        <input type="file" onChange={handleFileChange} />
         <br></br>
         <br></br>
         <button type="submit">上傳</button>
