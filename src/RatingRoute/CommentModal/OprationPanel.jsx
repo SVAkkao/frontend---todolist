da673aee56c7d314d9d132a420a8802a43e46c30
iigmir <roc120j@gmail.com>
2024-03-20 12:10:28 +0800

    <ChangelogList />

--- src/RatingRoute/CommentModal/OprationPanel.jsx
+++ src/RatingRoute/CommentModal/OprationPanel.jsx
@@ -2,12 +2,14 @@
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Table from "react-bootstrap/Table";
 // Fontawesome
 import { FaTrashCan, FaPen, FaClockRotateLeft } from "react-icons/fa6";
 // Local
 import CommentForm from "./CommentForm";
 import { modal_modules, modal_mode_modules } from "./utils";
-import { change_comment_api, delete_comment_api } from "./api";
+import { change_comment_api, delete_comment_api, get_comment_changelog_api } from "./api";
+import { useEffect, useState } from "react";
 
 function DeleteConfirmModal({ hideModal, iid }) {
     const close = () => {
@@ -24,8 +26,31 @@ function DeleteConfirmModal({ hideModal, iid }) {
 }
 
 function ChangelogList({ cid, closeChangelog }) {
+    const [changelogs, set_changelogs] = useState([]);
+    useEffect(() => {
+        get_comment_changelog_api(cid).then( response => {
+            set_changelogs(response.result);
+        });
+    }, [cid]);
     return <div className="changelog-list">
-        <p>{ cid }</p>
+        <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th>Changed date</th>
+                    <th>Before</th>
+                    <th>After</th>
+                </tr>
+            </thead>
+            <tbody>
+                { changelogs.map( (its) => <tr key={its.id}>
+                    <td>
+                        <time dateTime={its.created_at}>{its.created_at}</time>
+                    </td>
+                    <td>{its.before}</td>
+                    <td>{its.after}</td>
+                </tr>) }
+            </tbody>
+        </Table>
     </div>;
 }
 
@@ -33,6 +58,10 @@ export default function OprationPanel({ pid, cid, onDelete, onEdit, preloadDatas
     const { show, close_modal, show_modal } = modal_modules();
     const modalmode = modal_mode_modules();
     // Removing actions
+    function RemoveForm({ modalmode, closeRemoving }) {
+        const can_remove = modalmode.mode === modalmode.REMOVING;
+        return can_remove ? <DeleteConfirmModal hideModal={closeRemoving} /> : <div></div>;
+    }
     const open_removing = () => {
         modalmode.set_mode(modalmode.REMOVING);
         show_modal();
@@ -49,6 +78,10 @@ export default function OprationPanel({ pid, cid, onDelete, onEdit, preloadDatas
         });
     };
     // Editing actions
+    function EditForm({ modalmode, pid, closeEditing, preloadDatas }) {
+        const can_edit = modalmode.mode === modalmode.EDITING;
+        return can_edit ? <CommentForm pid={pid} submitAction={closeEditing} preloadDatas={preloadDatas} method="PUT" /> : <div></div>;
+    }
     const open_editing = () => {
         modalmode.set_mode(modalmode.EDITING);
         show_modal();
@@ -65,6 +98,10 @@ export default function OprationPanel({ pid, cid, onDelete, onEdit, preloadDatas
         });
     };
     // Changelog actions
+    function ChangelogForm({ modalmode, cid, closeChangelog }) {
+        const can_log = modalmode.mode === modalmode.CHANGELOG;
+        return can_log ? <ChangelogList cid={cid} closeChangelog={closeChangelog} /> : <div></div>;
+    }
     const open_changelog = () => {
         modalmode.set_mode(modalmode.CHANGELOG);
         show_modal();
@@ -73,18 +110,14 @@ export default function OprationPanel({ pid, cid, onDelete, onEdit, preloadDatas
         close_modal();
     };
     // HTML
-    function DeleteForm({ modalmode, closeRemoving }) {
-        const can_remove = modalmode.mode === modalmode.REMOVING;
-        return can_remove ? <DeleteConfirmModal hideModal={closeRemoving} /> : <div></div>;
-    }
-    function EditForm({ modalmode, pid, closeEditing, preloadDatas }) {
-        const can_edit = modalmode.mode === modalmode.EDITING;
-        return can_edit ? <CommentForm pid={pid} submitAction={closeEditing} preloadDatas={preloadDatas} method="PUT" /> : <div></div>;
-    }
-    function ChangelogForm({ modalmode, cid, closeChangelog }) {
-        const can_log = modalmode.mode === modalmode.CHANGELOG;
-        return can_log ? <ChangelogList cid={cid} closeChangelog={closeChangelog} /> : <div></div>;
-    }
+    const ModalTitle = ({ modalmode }) => {
+        switch (modalmode.mode) {
+            case modalmode.EDITING: return "編輯";
+            case modalmode.REMOVING: return "刪除";
+            case modalmode.CHANGELOG: return "修改記錄";
+            default: return "不明";
+        }
+    };
     return <div className="item-panel">
         <FaTrashCan className="click-icon" onClick={open_removing} />
         <FaPen className="click-icon" onClick={open_editing} />
@@ -92,10 +125,12 @@ export default function OprationPanel({ pid, cid, onDelete, onEdit, preloadDatas
         <div className="modals">
             <Modal id="edit-form-modal" size="xs" centered show={show} onHide={close_modal}>
                 <Modal.Header closeButton>
-                    <Modal.Title>編輯</Modal.Title>
+                    <Modal.Title>
+                        <ModalTitle modalmode={modalmode} />
+                    </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <DeleteForm modalmode={modalmode} closeRemoving={close_removing} />
+                    <RemoveForm modalmode={modalmode} closeRemoving={close_removing} />
                     <EditForm modalmode={modalmode} pid={pid} preloadDatas={preloadDatas} closeEditing={close_editing} />
                     <ChangelogForm modalmode={modalmode} cid={cid} closeChangelog={close_changelog} />
                 </Modal.Body>
