34587e8ea67ee8841203025d759d685f26be2e10
iigmir <roc120j@gmail.com>
2024-03-21 16:14:23 +0800

    Refactor: Moving modules

--- src/RatingRoute/CommentModal/CommentItem/OprationPanel.jsx
+++ src/RatingRoute/CommentModal/CommentItem/OprationPanel.jsx
@@ -62,11 +62,71 @@ function ChangelogList({ cid, closeChangelog }) {
     </div>;
 }
 
-export default function OprationPanel({ pid, cid, onDelete, onEdit, preloadDatas }) {
-    const { show, close_modal, show_modal } = modal_modules();
-    const modalmode = modal_mode_modules();
-    // Removing actions
-    function RemoveForm({ modalmode, closeRemoving }) {
+/**
+ * Changelog actions
+ * @param {*} modalmode 
+ * @param {*} show_modal 
+ * @param {*} close_modal 
+ * @returns 
+ */
+function changelog_modules(modalmode, show_modal, close_modal) {
+    const ChangelogForm = ({ modalmode, cid, closeChangelog }) => {
+        const can_log = modalmode.mode === modalmode.CHANGELOG;
+        return can_log ? <ChangelogList cid={cid} closeChangelog={closeChangelog} /> : <div></div>;
+    };
+    const open_changelog = () => {
+        modalmode.set_mode(modalmode.CHANGELOG);
+        show_modal();
+    };
+    const close_changelog = () => {
+        close_modal();
+    };
+    return { ChangelogForm, open_changelog, close_changelog };
+}
+
+/**
+ * Editing actions
+ * @param {*} modalmode 
+ * @param {*} show_modal 
+ * @param {*} close_modal 
+ * @param {*} cid 
+ * @param {*} onEdit 
+ * @returns 
+ */
+function edit_modules(modalmode, show_modal, close_modal, cid, onEdit) {
+    const EditForm = ({ modalmode, pid, closeEditing, preloadDatas }) => {
+        const can_edit = modalmode.mode === modalmode.EDITING;
+        return can_edit ? <CommentForm pid={pid} submitAction={closeEditing} preloadDatas={preloadDatas} method="PUT" /> : <div></div>;
+    };
+    const open_editing = () => {
+        modalmode.set_mode(modalmode.EDITING);
+        show_modal();
+    };
+    const close_editing = (form_dom) => {
+        const ajax = change_comment_api(form_dom, cid);
+        ajax.then((response) => {
+            console.log(response);
+            onEdit();
+            close_modal();
+        }).catch((response) => {
+            console.error(response);
+            alert(response.message);
+        });
+    };
+    return { EditForm, open_editing, close_editing };
+}
+
+/**
+ * Removing actions
+ * @param {*} modalmode 
+ * @param {*} show_modal 
+ * @param {*} close_modal 
+ * @param {*} cid 
+ * @param {*} onDelete 
+ * @returns 
+ */
+function remove_modules(modalmode, show_modal, close_modal, cid, onDelete) {
+    const RemoveForm = ({ modalmode, closeRemoving }) => {
         const can_remove = modalmode.mode === modalmode.REMOVING;
         return can_remove ? <DeleteConfirmModal hideModal={closeRemoving} /> : <div></div>;
     }
@@ -76,47 +136,25 @@ export default function OprationPanel({ pid, cid, onDelete, onEdit, preloadDatas
     };
     const close_removing = () => {
         const ajax = delete_comment_api(cid);
-        ajax.then( (response) => {
+        ajax.then((response) => {
             console.log(response);
             onDelete();
             close_modal();
-        }).catch( (response) => {
+        }).catch((response) => {
             console.error(response);
             alert(response.message);
         });
     };
-    // Editing actions
-    function EditForm({ modalmode, pid, closeEditing, preloadDatas }) {
-        const can_edit = modalmode.mode === modalmode.EDITING;
-        return can_edit ? <CommentForm pid={pid} submitAction={closeEditing} preloadDatas={preloadDatas} method="PUT" /> : <div></div>;
-    }
-    const open_editing = () => {
-        modalmode.set_mode(modalmode.EDITING);
-        show_modal();
-    };
-    const close_editing = (form_dom) => {
-        const ajax = change_comment_api(form_dom, cid);
-        ajax.then( (response) => {
-            console.log(response);
-            onEdit();
-            close_modal();
-        }).catch( (response) => {
-            console.error(response);
-            alert(response.message);
-        });
-    };
-    // Changelog actions
-    function ChangelogForm({ modalmode, cid, closeChangelog }) {
-        const can_log = modalmode.mode === modalmode.CHANGELOG;
-        return can_log ? <ChangelogList cid={cid} closeChangelog={closeChangelog} /> : <div></div>;
-    }
-    const open_changelog = () => {
-        modalmode.set_mode(modalmode.CHANGELOG);
-        show_modal();
-    };
-    const close_changelog = () => {
-        close_modal();
-    };
+    return { RemoveForm, open_removing, close_removing };
+}
+
+export default function OprationPanel({ pid, cid, onDelete, onEdit, preloadDatas }) {
+    const { show, close_modal, show_modal } = modal_modules();
+    const modalmode = modal_mode_modules();
+    // Removing actions
+    const { RemoveForm, open_removing, close_removing } = remove_modules(modalmode, show_modal, close_modal, cid, onDelete);
+    const { EditForm, open_editing, close_editing } = edit_modules(modalmode, show_modal, close_modal, cid, onEdit);
+    const { ChangelogForm, open_changelog, close_changelog } = changelog_modules(modalmode, show_modal, close_modal);
     // HTML
     const ModalTitle = ({ modalmode }) => {
         switch (modalmode.mode) {
