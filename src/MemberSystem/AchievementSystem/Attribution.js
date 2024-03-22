57f6f8b9fc7f13e90c434334416b02ab6bcad763
Areil0317 <qscft502832@gmail.com>
2024-03-22 10:10:38 +0800

    新增等級與積分公告

--- src/MemberSystem/AchievementSystem/Attribution.js
+++ src/MemberSystem/AchievementSystem/Attribution.js
@@ -14,19 +14,17 @@ import Divider from "@mui/material/Divider";
 import { blue } from "@mui/material/colors";
 import CommentIcon from "@mui/icons-material/Comment";
 import ListAltIcon from "@mui/icons-material/ListAlt";
-import Button from "@mui/material/Button";
-import CloseIcon from "@mui/icons-material/Close";
 import "./Attribution.css";
 import { FaMedal } from "react-icons/fa";
 import { MdGrade } from "react-icons/md";
-// import PointsModal from "./PointsModal";
+import Announce from "./Announce";
 
 const API_HOST = process.env.REACT_APP_API_URL;
 const API_IMAGE = process.env.REACT_APP_IMAGE_URL;
 
 const userData = {
   points: 1200,
-  level: "LV.3‧探險家",
+  level: "LV.3 探險家",
   contributions: [
     { type: "to-do-list", content: "访问故宫" },
     { type: "comment", content: "这里的风景真是太美了！" },
@@ -95,6 +93,7 @@ const ContributionsPanel = ({ contributions, comments, filter }) => {
 const UserIntroduction = () => {
   const [userName, setUserName] = useState("");
   const [userPhoto, setUserPhoto] = useState("");
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   const fetchUser = async () => {
     try {
@@ -109,29 +108,6 @@ const UserIntroduction = () => {
     fetchUser();
   }, []);
 
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-
-  const grade = [
-    { name: "創建清單", points: "5積分" },
-    { name: "新增景點", points: "5積分" },
-    { name: "新增景點中的活動", points: "5積分" },
-    { name: "新增相片", points: "20積分" },
-    { name: "新增評論", points: "20積分" },
-  ];
-
-  const levels = [
-    { name: "LV.1 新手旅行者", points: "0積分" },
-    { name: "LV.2 城市漫步者", points: "500積分" },
-    { name: "LV.3 探險家", points: "1,500積分" },
-    { name: "LV.4 文化收藏家", points: "3,000積分" },
-    { name: "LV.5 國家征服者", points: "5,000積分" },
-    { name: "LV.6 環球旅行者", points: "8,000積分" },
-    { name: "LV.7 冒險家", points: "12,000積分" },
-    { name: "LV.8 旅行達人", points: "17,000積分" },
-    { name: "LV.9 旅行大使", points: "23,000積分" },
-    { name: "LV.10 世界公民", points: "30,000積分" },
-  ];
-
   return (
     <CardContent>
       <br />
@@ -145,77 +121,7 @@ const UserIntroduction = () => {
         </Typography>
       </div>
       <br />
-      <h5>
-        <MdGrade
-          style={{ margin: "10px", color: "#FFD700", cursor: "pointer" }}
-          onClick={() => setModalIsOpen(true)}
-        />
-        積分：{userData.points}
-      </h5>
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={() => setModalIsOpen(false)}
-        style={{
-          content: {
-            padding: "40px",
-            top: "50%",
-            left: "50%",
-            right: "auto",
-            bottom: "auto",
-            marginRight: "-50%",
-            transform: "translate(-50%, -50%)",
-            width: "500px", // 自定义宽度
-            height: "600px", // 自定义高度
-          },
-          overlay: {
-            backgroundColor: "rgba(0, 0, 0, 0.75)", // 可以调整遮罩层样式
-          },
-        }}
-      >
-        <Button
-          style={{
-            position: "absolute",
-            top: "10px",
-            right: "10px",
-            minWidth: "auto", // 最小宽度调整为auto，防止按钮过宽
-          }}
-          onClick={() => setModalIsOpen(false)}
-        >
-          <CloseIcon />
-        </Button>
-        <h3>
-          <MdGrade style={{ margin: "10px", color: "#FFD700" }} />
-          積分說明
-        </h3>
-        <ul>
-          {grade.map((grade, index) => (
-            <li
-              key={index}
-              style={{ fontSize: "20px", margin: "2px" }}
-            >{`${grade.name} - ${grade.points}`}</li>
-          ))}
-        </ul>
-        <br></br>
-        <h3>
-          <FaMedal style={{ margin: "10px", color: "#C0C0C0" }} />
-          等級說明：
-        </h3>
-        <ul>
-          {levels.map((level, index) => (
-            <li
-              key={index}
-              style={{ fontSize: "20px", margin: "2px" }}
-            >{`${level.name} - ${level.points}`}</li>
-          ))}
-        </ul>
-      </Modal>
-      <h5>
-        <FaMedal
-          style={{ margin: "10px", color: "#C0C0C0", cursor: "pointer" }}
-          onClick={() => setModalIsOpen(true)}
-        />
-        等級：{userData.level}
-      </h5>
+      <Announce></Announce>
     </CardContent>
   );
 };
