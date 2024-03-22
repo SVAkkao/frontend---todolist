94098f06391d683299db0de9eade43adefbd265e
SVAkkao <qaws4256@gmail.com>
2024-03-22 15:20:32 +0800

    list index

--- src/UserListThings/Pages/Outdoor.js
+++ src/UserListThings/Pages/Outdoor.js
@@ -8,7 +8,7 @@ function Outdoor() {
             <Row>
                 <Col>
                     <Card style={{ width: '18rem' }} className="mb-4 text-center">
-                        <Card.Img variant="top" src="https://img.freepik.com/free-photo/lonely-figure-adventurer-explorer-with-big-backpack-drone_346278-238.jpg?t=st=1711086330~exp=1711089930~hmac=b0dfef4dfa5b890a4b1dcf667f02a148db1e760acd725994bef9932b064dc012&w=1380" />
+                        <Card.Img variant="top" src="/UserListSource/outdoor1.avif" />
                         <Card.Body>
                             <Card.Title>登山步行</Card.Title>
                         </Card.Body>
@@ -16,7 +16,7 @@ function Outdoor() {
                 </Col>
                 <Col>
                     <Card style={{ width: '18rem' }}  className="mb-4 text-center">
-                        <Card.Img variant="top" src="https://img.freepik.com/free-photo/close-up-legs-sneakers-hands-steering-wheel-hipster-style-bearded-man-red-hoodie-beige-trousers-riding-alone-with-backpack-bicycle-healthy-active-lifestyle-traveler-backpacker_285396-5493.jpg?t=st=1711076873~exp=1711080473~hmac=1d66e3e4bece17875b23f65ac623739c8fd578350c2a493c50decd544aa325eb&w=1380" />
+                        <Card.Img variant="top" src="/UserListSource/outdoor2.avif" />
                         <Card.Body>
                             <Card.Title>自行車旅行</Card.Title>
                         </Card.Body>
@@ -24,7 +24,7 @@ function Outdoor() {
                 </Col>
                 <Col>
                     <Card style={{ width: '18rem' }}  className="mb-4 text-center">
-                        <Card.Img variant="top" src="https://img.freepik.com/free-photo/look-from-snowboarder-red-jacket-mountain-hills-covered-with-snow_8353-1052.jpg?t=st=1711076980~exp=1711080580~hmac=19a6d0f0abb934a63cc2a9aa3a854775aed12b47827cf97a291116e46fec0ba0&w=1380" />
+                        <Card.Img variant="top" src="/UserListSource/outdoor3.avif" />
                         <Card.Body>
                             <Card.Title>雪山滑雪</Card.Title>
                         </Card.Body>
@@ -32,7 +32,7 @@ function Outdoor() {
                 </Col>
                 <Col>
                     <Card style={{ width: '18rem' }}  className="mb-4 text-center">
-                        <Card.Img variant="top" src="https://img.freepik.com/free-photo/senior-male-coach-teaching-young-sportsman-how-play-golf_171337-6507.jpg?t=st=1711077068~exp=1711080668~hmac=8eff6047544a333ba6579a00c7bff92ebeb74bf81d9e3be1613e3d2cec6b4a04&w=1380" />
+                        <Card.Img variant="top" src="/UserListSource/outdoor4.avif" />
                         <Card.Body>
                             <Card.Title>高爾夫球</Card.Title>
                         </Card.Body>
@@ -40,7 +40,7 @@ function Outdoor() {
                 </Col>
                 <Col>
                     <Card style={{ width: '18rem' }}  className="mb-4 text-center">
-                        <Card.Img variant="top" src="https://img.freepik.com/free-photo/closeup-man-parachuting-near-ground-with-red-parachute-daylight_181624-43606.jpg?t=st=1711077137~exp=1711080737~hmac=c0f79846ab96104440cfb07febe331b5a515d56076ae821ccf1a9b4f8232a536&w=1380" />
+                        <Card.Img variant="top" src="/UserListSource/outdoor5.avif" />
                         <Card.Body>
                             <Card.Title>高空跳傘</Card.Title>
                         </Card.Body>
@@ -48,7 +48,7 @@ function Outdoor() {
                 </Col>
                 <Col>
                     <Card style={{ width: '18rem' }}  className="mb-4 text-center">
-                        <Card.Img variant="top" src="https://img.freepik.com/free-photo/yellow-truck-riding-sandy-ground-cloudy-blue-sky_181624-17240.jpg?t=st=1711086280~exp=1711089880~hmac=9cb2ea445c43752c4ce479199deeb980921c60681d19f8d2bf1675f964e443aa&w=1380" />
+                        <Card.Img variant="top" src="/UserListSource/outdoor6.avif" />
                         <Card.Body>
                             <Card.Title>沙上駕車</Card.Title>
                         </Card.Body>
@@ -56,7 +56,7 @@ function Outdoor() {
                 </Col>
                 <Col>
                     <Card style={{ width: '18rem' }}  className="mb-4 text-center">
-                        <Card.Img variant="top" src="https://img.freepik.com/free-photo/cowboy-silhouette-with-horse-against-warm-light_23-2149334126.jpg?t=st=1711077224~exp=1711080824~hmac=41f8ce780d14bb90c29a52ed0e73933bf1a0a19afe16915c7375a269174508de&w=1380" />
+                        <Card.Img variant="top" src="/UserListSource/outdoor7.avif" />
                         <Card.Body>
                             <Card.Title>馬背騎行</Card.Title>
                         </Card.Body>
@@ -64,7 +64,7 @@ function Outdoor() {
                 </Col>
                 <Col>
                     <Card style={{ width: '18rem' }}  className="mb-4 text-center">
-                        <Card.Img variant="top" src="https://img.freepik.com/free-photo/barbecue-grill-party-tasty-food-wooden-desk_176420-1839.jpg?t=st=1711077336~exp=1711080936~hmac=f63405a6dfef7f3ef7c47f8bb1c4236e998bdc886aed8574e48e3b7dd7bf7195&w=1380" />
+                        <Card.Img variant="top" src="/UserListSource/outdoor8.avif" />
                         <Card.Body>
                             <Card.Title>野外烤肉</Card.Title>
                         </Card.Body>
@@ -72,7 +72,7 @@ function Outdoor() {
                 </Col>
                 <Col>
                     <Card style={{ width: '18rem' }}  className="mb-4 text-center">
-                        <Card.Img variant="top" src="https://img.freepik.com/free-photo/overhead-view-female-friends-enjoying-picnic_23-2147908164.jpg?t=st=1711077388~exp=1711080988~hmac=2485d6d814167375be363b58c87c1b4f2be678169895a9227d26e16e6f4e46a4&w=1380" />
+                        <Card.Img variant="top" src="/UserListSource/outdoor9.avif" />
                         <Card.Body>
                             <Card.Title>公園野餐</Card.Title>
                         </Card.Body>
@@ -80,7 +80,7 @@ function Outdoor() {
                 </Col>
                 <Col>
                     <Card style={{ width: '18rem' }}  className="mb-4 text-center">
-                        <Card.Img variant="top" src="https://img.freepik.com/free-photo/closeup-shot-sparrow-bird-perched-green-plants_181624-52804.jpg?t=st=1711077451~exp=1711081051~hmac=eea512e8c38597e0798ab41922cf0564b2ed5e3192f01e810e815dcf47ec0e06&w=1380" />
+                        <Card.Img variant="top" src="/UserListSource/outdoor10.avif" />
                         <Card.Body>
                             <Card.Title>戶外賞鳥</Card.Title>
                         </Card.Body>
@@ -88,7 +88,7 @@ function Outdoor() {
                 </Col>
                 <Col>
                     <Card style={{ width: '18rem' }}  className="mb-4 text-center">
-                        <Card.Img variant="top" src="https://img.freepik.com/free-photo/silhouette-people-enjoying-their-time-sunset-cafe-potamos-beach-greece_181624-27427.jpg?t=st=1711077494~exp=1711081094~hmac=260a76b73f2d9a53bd1f28ff0303e91fa9d792cfc642b4ffb7c7e81b6b40c837&w=1380" />
+                        <Card.Img variant="top" src="/UserListSource/outdoor11.avif" />
                         <Card.Body>
                             <Card.Title>觀賞日落</Card.Title>
                         </Card.Body>
@@ -96,7 +96,7 @@ function Outdoor() {
                 </Col>
                 <Col>
                     <Card style={{ width: '18rem' }}  className="mb-4 text-center">
-                        <Card.Img variant="top" src="https://img.freepik.com/free-photo/man-spacesuit-is-walking-abandoned-area_651396-3521.jpg?t=st=1711087293~exp=1711090893~hmac=09ad841c5a22e4aae77cd2eae52aebf627ab5268e9d795cbd4c25d64edfb4014&w=1380" />
+                        <Card.Img variant="top" src="/UserListSource/outdoor12.avif" />
                         <Card.Body>
                             <Card.Title>觀測星空</Card.Title>
                         </Card.Body>
