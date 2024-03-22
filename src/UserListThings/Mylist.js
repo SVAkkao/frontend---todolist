804bf49ccd2ff2d1abfb91e1d3fef3eb7b63b844
SVAkkao <qaws4256@gmail.com>
2024-03-22 14:16:25 +0800

    list index

--- src/UserListThings/Mylist.js
+++ src/UserListThings/Mylist.js
@@ -1,38 +1,38 @@
 import React from 'react'
-import {Row, Col, Card, Button} from 'react-bootstrap';
+import { Row, Col, Card, Button } from 'react-bootstrap';
 
-function Mylist({data, onButtonClick}) {
+function Mylist({ data, onButtonClick }) {
 
     const startDate = new Date(data.start_date).toLocaleDateString();
     const endDate = new Date(data.end_date).toLocaleDateString();
 
-  return (
-    <Row className='m-5'>
-    <Col>
-        <Card className='mt-4'>
-            <Card.Body style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', backgroundColor: '#AAD9BB', textAlign: 'center' }}>
-                <div style={{ flex: '1', textAlign: 'center' }}>
-                    <Card.Text className='text3'>
-                    {data.title}
-                    </Card.Text>
-                </div>
-                <div>
+    return (
+        <Row className='m-5'>
+            <Col>
+                <Card className='mt-4'>
+                    <Card.Body style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', backgroundColor: '#AAD9BB', textAlign: 'center' }}>
+                        <div style={{ flex: '1', textAlign: 'center' }}>
+                            <Card.Text className='text3'>
+                                {data.title}
+                            </Card.Text>
+                        </div>
+                        <div>
                             <Button variant="light" onClick={() => onButtonClick(data.tlid)}>選擇</Button>
-                </div>
-                <div>
-                    <img style={{ width: "20px", height: '20px', paddingBottom: '0' }} src="/UserListSource/delete.png" alt="Icon" />
-                </div>
-            </Card.Body>
-            <Card.Img variant="bottom" src="https://images.pexels.com/photos/17573850/pexels-photo-17573850.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" style={{ border: 'grey', width: '100%', height: '200px' }} />
-        </Card>
-    </Col>
-    <Row className='mt-3' style={{ alignItems: 'center' }}>
-        <Col><p className='text4'>{startDate}</p></Col>
-        <Col><img style={{ width: "24px", height: '24px', marginBottom: '12px', paddingBottom: '0' }} src="/UserListSource/to.png" alt="Icon" /></Col>
-        <Col><p className='text4'>{endDate}</p></Col>
-    </Row>
-</Row>
-  )
+                        </div>
+                        <div>
+                            <img style={{ width: "20px", height: '20px', paddingBottom: '0' }} src="/UserListSource/delete.png" alt="Icon" />
+                        </div>
+                    </Card.Body>
+                    <Card.Img variant="bottom" src="/UserListSource/Mylist.webp" style={{ border: 'grey', width: '100%', height: '200px' }} />
+                </Card>
+            </Col>
+            <Row className='mt-3' style={{ alignItems: 'center' }}>
+                <Col><p className='text4'>{startDate}</p></Col>
+                <Col><img style={{ width: "24px", height: '24px', marginBottom: '12px', paddingBottom: '0' }} src="/UserListSource/to.png" alt="Icon" /></Col>
+                <Col><p className='text4'>{endDate}</p></Col>
+            </Row>
+        </Row>
+    )
 }
 
 export default Mylist
