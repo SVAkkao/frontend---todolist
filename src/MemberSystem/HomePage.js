2fdb7c6558d622394863f877f8cbd0e4ce7cdb79
Areil0317 <qscft502832@gmail.com>
2024-03-22 15:28:49 +0800

    修復rwd

--- src/MemberSystem/HomePage.js
+++ src/MemberSystem/HomePage.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./LoginSystem.css";
 
 function NavList() {
