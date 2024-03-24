4af815f9442fcb73ae54bc8522006b61daeecef5
iigmir <roc120j@gmail.com>
2024-03-25 00:52:14 +0800

    WTFM

--- src/stores/user.js
+++ src/stores/user.js
@@ -13,6 +13,27 @@ const USER_DEFAULT = {
     "cellphone": ""
 };
 
+/**
+ * 一般來說，如果您使用了 `src/MemberSystem` 的 `<LogoutBar />` 元件，則不需要呼叫 `getUser` 方法，因為該元件呼叫了它。
+ * 您只需要存取 `user` 和 `getUserPhoto` （如果您需要訪問代表 `user.photo` 的照片）：
+ * 
+ * ```js
+ * const Example = () => {
+ *     const { user, getUserPhoto } = useUserStore();
+ *     return <div> <img src={getUserPhoto()} alt="Photo" /> {user.name}'s photo </div>
+ * };
+ * ```
+ * 
+ * 如果您因為某些原因沒使用 <LogoutBar /> 元件，您可能需要附加並呼叫 `getUser` 方法：
+ * 
+ * ```js
+ * const Example = () => {
+ *     const { user, getUser, getUserPhoto } = useUserStore();
+ *     useEffect( () => { getUser(); }, []);
+ *     return <div> <img src={getUserPhoto()} alt="Photo" /> {user.name}'s photo </div>
+ * };
+ * ```
+ */
 export const useUserStore = create((set, get) => ({
     /**
      * 使用者資料
