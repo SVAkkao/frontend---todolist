d933d0fcc508feeb96f38f1d768e5a61e0a28383
iigmir <roc120j@gmail.com>
2024-03-25 23:03:35 +0800

    WTFM

--- src/MemberSystem/AchievementSystem/api.js
+++ src/MemberSystem/AchievementSystem/api.js
@@ -5,9 +5,9 @@ const API_HOST = process.env.REACT_APP_API_URL;
 export const getUserScore = () => {
   const userToken = localStorage.getItem("userToken");
   return fetch(`${API_HOST}/api/user-score`, {
-    method: "GET", // 或者是POST，取決於你的API
+    method: "GET",
     headers: {
-      Authorization: `Bearer ${userToken}`, // 根據你的API設計調整這裡,
+      Authorization: `Bearer ${userToken}`,
       Accept: "application/json",
     },
   }).then((response) => response.json());
