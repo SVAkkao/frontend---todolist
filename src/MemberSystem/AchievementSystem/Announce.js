dd46bf90fcf0e78e6d52a3f9dc1934161bffac18
Areil0317 <qscft502832@gmail.com>
2024-03-25 12:55:34 +0800

    更新成就名稱

--- src/MemberSystem/AchievementSystem/Announce.js
+++ src/MemberSystem/AchievementSystem/Announce.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "@mui/material/Button";
 import CloseIcon from "@mui/icons-material/Close";
 // import Modal from "@mui/material/Modal";
@@ -6,8 +6,35 @@ import Modal from "react-modal";
 import { MdGrade } from "react-icons/md"; // 确保您已经安装了react-icons
 import { FaMedal } from "react-icons/fa";
 
+const API_HOST = process.env.REACT_APP_API_URL;
+
 function Announce() {
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [score, setScore] = useState(null); // 狀態用於儲存分數
+  const [achievement, setAchievement] = useState("Loading...");
+
+  useEffect(() => {
+    const userToken = localStorage.getItem("userToken"); // 從localStorage獲取userToken
+    if (userToken) {
+      fetch(`${API_HOST}/api/user-score`, {
+        method: "GET", // 或者是POST，取決於你的API
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userToken}`, // 根據你的API設計調整這裡
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setScore(data.totalScore); // 假設API響應中有一個score字段
+          const userAchievement = getUserAchievement(data.totalScore);
+          // 更新成就状态
+          setAchievement(userAchievement);
+        })
+        .catch((error) => {
+          console.error("Error fetching score:", error);
+        });
+    }
+  }, []);
 
   const grade = [
     { name: "創建清單", points: "5積分" },
@@ -19,16 +46,42 @@ function Announce() {
 
   const levels = [
     { name: "LV.1 新手旅行者", points: "0積分" },
-    { name: "LV.2 城市漫步者", points: "500積分" },
-    { name: "LV.3 探險家", points: "1,500積分" },
-    { name: "LV.4 文化收藏家", points: "3,000積分" },
-    { name: "LV.5 國家征服者", points: "5,000積分" },
-    { name: "LV.6 環球旅行者", points: "8,000積分" },
-    { name: "LV.7 冒險家", points: "12,000積分" },
-    { name: "LV.8 旅行達人", points: "17,000積分" },
-    { name: "LV.9 旅行大使", points: "23,000積分" },
+    { name: "LV.2 城市漫步者", points: "100積分" },
+    { name: "LV.3 探險家", points: "1,000積分" },
+    { name: "LV.4 文化收藏家", points: "2,000積分" },
+    { name: "LV.5 國家征服者", points: "3,000積分" },
+    { name: "LV.6 環球旅行者", points: "5,000積分" },
+    { name: "LV.7 冒險家", points: "10,000積分" },
+    { name: "LV.8 旅行達人", points: "15,000積分" },
+    { name: "LV.9 旅行大使", points: "20,000積分" },
     { name: "LV.10 世界公民", points: "30,000積分" },
   ];
+
+  function getUserAchievement(score) {
+    if (score >= 30000) {
+      return "LV.10 世界公民";
+    } else if (score >= 20000) {
+      return "LV.9 旅行大使";
+    } else if (score >= 15000) {
+      return "LV.8 旅行達人";
+    } else if (score >= 10000) {
+      return "LV.7 冒險家";
+    } else if (score >= 5000) {
+      return "LV.6 環球旅行者";
+    } else if (score >= 3000) {
+      return "LV.5 國家征服者";
+    } else if (score >= 2000) {
+      return "LV.4 文化收藏家";
+    } else if (score >= 1000) {
+      return "LV.3 探險家";
+    } else if (score >= 100) {
+      return "LV.2 城市漫步者";
+    } else if (score < 100) {
+      return "LV.1 新手旅行者";
+    } else {
+      return "loading";
+    }
+  }
 
   return (
     <>
@@ -37,7 +90,7 @@ function Announce() {
         onClick={() => setModalIsOpen(true)}
       >
         <MdGrade style={{ margin: "10px", color: "#FFD700" }} />
-        積分：
+        積分：{score}
       </span>
       <br></br>
       <span
@@ -45,7 +98,7 @@ function Announce() {
         onClick={() => setModalIsOpen(true)}
       >
         <FaMedal style={{ margin: "10px", color: "#C0C0C0" }} />
-        等級：
+        等級：{achievement}
       </span>
 
       <Modal
