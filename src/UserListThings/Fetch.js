1b741af0c530377456d0fdd7528429f51e56a819
iigmir <roc120j@gmail.com>
2024-03-26 20:00:26 +0800

    API! API!

--- src/UserListThings/Fetch.js
+++ src/UserListThings/Fetch.js
@@ -25,7 +25,7 @@ const ajaxUserList = async () => {
 };
 
 const getUserrelatedids = (body = {}) => {
-  return fetch(API_HOST + '/api/POST/userrelatedids', {
+  return fetch(`${API_HOST}/api/POST/userrelatedids`, {
     method: 'POST',
     headers: getRequestHeaders(),
     body: body
@@ -53,15 +53,9 @@ const getSelectIds = (selectlistBodies = []) => {
 function Fetch({ onSelect2 }) {
   const [data2, setData2] = useState([]);
   const [listdata, FetchsetListData] = useState(null);
-
   if (onSelect2 && typeof onSelect2 === 'function') {
     onSelect2(listdata)
   }
-
-  const onSelect1 = (tlid) => {
-    FetchsetListData(tlid)
-  };
-
   function ajaxAction(setData2) {
     ajaxUserList().then(data => {
       // 繼續使用userId來發送下一個HTTP請求
@@ -71,9 +65,7 @@ function Fetch({ onSelect2 }) {
       getUserrelatedids(body).then(data => {
         const selectlistBodies = data.tlid.map(tlid => ({ tlid }));
         const getSelectLists = getSelectIds(selectlistBodies);
-        // const selectlistBody = JSON.stringify({tlid: tlid[0] })
-        // setTestData(selectlistBodies)
-        getSelectLists.then(data => {
+        getSelectLists.then( (data = []) => {
           setData2(data);
         }).catch(error => console.error(error));
       }).catch(error => console.error(error));
@@ -82,18 +74,11 @@ function Fetch({ onSelect2 }) {
   useEffect(() => {
     ajaxAction(setData2);
   }, []);
-
-  // useEffect(() => {
-  // console.log(testdata)
-  // 在這裡處理返回的資料
-  // }, [testdata]); // 添加data1為依賴項，以在data1更新時執行此回調函數
-
-  // useEffect(() => {
-  // console.log(data2)
-  // 在這裡處理返回的資料
-  // }, [data2]); 
-
-  return <LeftSide data={data2} onSelect={onSelect1} update_info={() => ajaxAction(setData2)} />;
+  return <LeftSide
+    data={data2}
+    onSelect={(tlid) => FetchsetListData(tlid)}
+    update_info={() => ajaxAction(setData2)}
+  />;
 }
 
 export default Fetch;
