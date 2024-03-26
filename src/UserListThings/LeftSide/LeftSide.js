defa836ce65222009f44f8f155a9a51da9e753fa
iigmir <roc120j@gmail.com>
2024-03-26 22:00:51 +0800

    Extract AJAX out

--- src/UserListThings/LeftSide/LeftSide.js
+++ src/UserListThings/LeftSide/LeftSide.js
@@ -4,39 +4,7 @@ import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
 import Mylist from "./Mylist";
 import { useUserStore } from "../../stores/user";
-
-// AJAX APIs
-const API_HOST = process.env.REACT_APP_API_URL;
-// const API_IMAGE = process.env.REACT_APP_IMAGE_URL
-async function ajaxAddList() {
-  const token = localStorage.getItem("userToken");
-  const r = await fetch(`${API_HOST}/api/POST/addlist`, {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      title: "請輸入旅行名稱..."
-    })
-  });
-  return await r.json();
-}
-async function ajaxRemoveList(tlid = 0) {
-  const token = localStorage.getItem("userToken");
-  const r = await fetch(`${API_HOST}/api/POST/deletelist`, {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ tlid })
-  });
-  if (r.status === 204) {
-    return {};
-  } 
-  return await r.json();
-}
+import { ajaxAddList, ajaxRemoveList } from "./api";
 
 // Components
 function TripLists({ list, finishedSelected, onButtonClick, onRemove }) {
