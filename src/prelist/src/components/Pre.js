60105be66cd39a82cfc5871259cbe2af6827c5d3
Areil0317 <qscft502832@gmail.com>
2024-03-26 14:04:37 +0800

    Use react-icons/fa

--- src/prelist/src/components/Pre.js
+++ src/prelist/src/components/Pre.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import { FaPenSquare, FaTrash } from "react-icons/fa";
 
 export const Pre = ({ task, deletePre, editPre, toggleComplete }) => {
   return (
     <div className="Pre">
-      <p className={`${task.completed ? "completed" : "incompleted"}`} onClick={() => toggleComplete(task.id)}>{task.task}</p>
+      <p
+        className={`${task.completed ? "completed" : "incompleted"}`}
+        onClick={() => toggleComplete(task.id)}
+      >
+        {task.task}
+      </p>
       <div>
-        <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} onClick={() => editPre(task.id)} />
-        <FontAwesomeIcon className="delete-icon" icon={faTrash} onClick={() => deletePre(task.id)} />
+        <FaPenSquare className="edit-icon" onClick={() => editPre(task.id)} />
+        <FaTrash className="delete-icon" onClick={() => deletePre(task.id)} />
       </div>
     </div>
-  )
-}
+  );
+};
