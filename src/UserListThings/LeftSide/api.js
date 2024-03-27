63547663a7fcefa35d199f20c2ef1170368f4c5c
Mingzhen0814 <fcum1102930@gmail.com>
2024-03-27 13:31:57 +0800

    after argument

--- src/UserListThings/LeftSide/api.js
+++ src/UserListThings/LeftSide/api.js
@@ -12,15 +12,45 @@ export async function ajaxAddList() {
             title: "請輸入旅行名稱..."
         })
     });
-    return await r.json();
+
+    return r.json();
+
+    // fetch(`${API_HOST}/api/POST/addlist`, {
+    //     method: "POST",
+    //     headers: {
+    //         Authorization: `Bearer ${token}`,
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify({
+    //         title: "請輸入旅行名稱..."
+    //     })
+    //   })
+    //   .then(async (response) => {
+    //     const tlidResult = await response.json();
+    //     console.log(tlidResult);
+    //     console.log(tlidResult.result.tlid);
+    //     fetch(`${API_HOST}/api/POST/addjourney`, {
+    //         method: "POST",
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify({
+    //             tlid : tlidResult.result.tlid,
+    //             aname: "輸入您的第一個行程..."
+    //         })
+    //     });
+    //   })
+    //   .catch((error) => {
+    //     console.error('Error:', error);
+    //   });
 }
 export async function ajaxRemoveList(tlid = 0) {
     const token = localStorage.getItem("userToken");
     const r = await fetch(`${API_HOST}/api/POST/deletelist`, {
         method: "POST",
         headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json'
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify({ tlid })
     });
