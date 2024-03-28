d9a17da2ca915d496e83a20d822c18d55c02557d
iigmir <roc120j@gmail.com>
2024-03-28 10:02:40 +0800

    Refactor LeftSide APIs

--- src/UserListThings/LeftSide/api.js
+++ src/UserListThings/LeftSide/api.js
@@ -1,65 +1,55 @@
 const API_HOST = process.env.REACT_APP_API_URL;
 
-export async function ajaxAddList() {
+function generateRequestingHeaders() {
     const token = localStorage.getItem("userToken");
-    // const r = await fetch(`${API_HOST}/api/POST/addlist`, {
-    //     method: "POST",
-    //     headers: {
-    //         Authorization: `Bearer ${token}`,
-    //         'Content-Type': 'application/json'
-    //     },
-    //     body: JSON.stringify({
-    //         title: "請輸入旅行名稱..."
-    //     })
-    // });
+    return {
+        Authorization: `Bearer ${token}`,
+        Accept: "application/json",
+        "Content-Type": "application/json"
+    };
+}
 
-    // return r.json();
+export function ajaxAddJourney(body = { tlid: 0, aname: "輸入您的第一個行程..." }) {
+    fetch(`${API_HOST}/api/POST/addjourney`, {
+        method: "POST",
+        headers: generateRequestingHeaders(),
+        body: JSON.stringify(body)
+    });
+}
 
+export async function ajaxAddList() {
     fetch(`${API_HOST}/api/POST/addlist`, {
         method: "POST",
-        headers: {
-            Authorization: `Bearer ${token}`,
-            'Content-Type': 'application/json'
-        },
+        headers: generateRequestingHeaders(),
         body: JSON.stringify({
             title: "請輸入旅行名稱..."
         })
-      })
-      .then(async (response) => {
+    })
+    .then(async (response) => {
         const tlidResult = await response.json();
-        console.log(tlidResult);
-        console.log(tlidResult.result.tlid);
-        fetch(`${API_HOST}/api/POST/addjourney`, {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                tlid : tlidResult.result.tlid,
-                aname: "輸入您的第一個行程..."
-            })
+        console.log(tlidResult, tlidResult.result.tlid);
+        ajaxAddJourney({
+            tlid: tlidResult.result.tlid,
+            aname: "輸入您的第一個行程..."
         });
-      })
-      .catch((error) => {
+    })
+    .catch((error) => {
         console.error('Error:', error);
-      });
-}
+    });
+};
+
 export async function ajaxRemoveList(tlid = 0) {
     console.log(tlid);
-    const token = localStorage.getItem("userToken");
-    const r = await fetch(`${API_HOST}/api/POST/deletelist`, {
+    const response = await fetch(`${API_HOST}/api/POST/deletelist`, {
         method: "POST",
-        headers: {
-            Authorization: `Bearer ${token}`,
-            'Content-Type': 'application/json'
-        },
+        headers: generateRequestingHeaders(),
         body: JSON.stringify({ 
             tlid
         })
     });
-    if (r.status === 204) {
+    if (response.status === 204) {
         return {};
     }
-    return await r.json();
+    return await response.json();
 }
 
